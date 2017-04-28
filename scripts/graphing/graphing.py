9c9fdad4da9fea9cb27b68142808c70d41d1617b
Markus Pfundstein <mpfundstein@protonmail.com>
2017-04-28 19:52:34 +0200

    possibility to edit only one dude

--- scripts/graphing/graphing.py
+++ scripts/graphing/graphing.py
@@ -15,7 +15,24 @@ def read_json_file(j):
     with open(j) as json_data:
         return json.load(json_data)
         
-def plot_objective_values(ppa_data, fwa_data, run, function_name):
+def plot_objective_values(data, run, alg_name, function_name, to=-1):
+    
+    objective_values = data[run]['bestMembersInGeneration']['values']
+    if to != -1:
+        objective_values = objective_values[:to]
+    
+    plt.plot(
+            objective_values, 
+            "g")
+    
+    plt.legend(['alg_name'])
+    plt.title(function_name)
+    plt.ylabel('f')
+    plt.xlabel('generation')
+    plt.show()
+    
+    
+def plot_ppa_vs_fwa(ppa_data, fwa_data, run, function_name):
     
     ppa_objective_values = ppa_data[run]['bestMembersInGeneration']['values']
     fwa_objective_values = fwa_data[run]['bestMembersInGeneration']['values']
